fda443a80dc42cbe3ddc6cf59a646d43334bd42f
dinodajin <djbrachio@gmail.com>
2025-02-25 17:34:38 +0900

    Initial settings

---
+++ backend/src/main/java/com/example/motoo/MotooApplication.java
@@ -0,0 +1,13 @@
+package com.example.motoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotooApplication.class, args);
+	}
+
+}
